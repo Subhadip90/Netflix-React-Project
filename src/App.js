1eeba410025a3970519516acebecdb32f3d0860a
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-23 15:21:47 +0000

    Netflix Cards React Project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+function Card(props) {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <div className='cards'>
+        <div className="card">
+          <img src={props.imgsrc} alt='myPic' className='card_img' />
+          <div className="card_info">
+            <span className="card_category">{props.sname}</span>
+            <h3 className="card_title">{props.title}</h3>
+            <a href={props.link} target={'_blank'}>
+              <button>Watch Now</button>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  return (
+    <>
+      <Card imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
+        title="A Netflix Original Series"
+        sname="Money Heist"
+        link="https://www.netflix.com/in/title/80192098" />
+      <Card />
+      <Card />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Sdata.jsx
@@ -0,0 +1,45 @@
+const Sdata = [
+  {
+    sname: "Money Heist",
+    imgsrc: "https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80192098"
+  },
+
+  {
+    sname: "Stranger Things",
+    imgsrc: "https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSNtJvu8215Gl8GlVv_EEDFMgltlMgvQGUb_8wpfUL6BKCLHa5PYQcZZVizJJhcTCUsy4ST5mhGNGGffm-MYgGgdgvIGJf_OvT0IPUEke88IaN7ireJ4uV_BVLjVTXbwlfo1.jpg?r=c68",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80057281"
+  },
+
+  {
+    sname: "Squid Game",
+    imgsrc: "https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZemmrMaRP3BbvSmgKLTfo1M3yb8VgbYQQUgc3pn-Zgr4WoPJRLV4zr4Kyq6nXqGcaFbPaRcamPUw6isYOw-pDhDcnhh4crH5qlVp-eVbnQHxEGpfaH60OKmweG7cYjLz9am.jpg?r=853",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/81040344"
+  },
+
+  {
+    sname: "Sacred Games",
+    imgsrc: "https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXoVIEQMjFVXwF9yNcUb4GNLm2aRtrrLI9Wdou7LoeDIyVwQjuCfqd_jiypxcwRqDbT5niScKPDOcG35VsgWJf72Iq_AoExpwr2LDiA1C_mQhGA6e6JZf7YJ3xs3DjnxpE9T.jpg?r=f89",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80115328"
+  },
+
+  {
+    sname: "The Crown",
+    imgsrc: "https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQlzgVGheFfNnJ9jmktkrccLKqf7KDJRDpMof3bkv9XITYv9CLB52-P1NaEb4I7HQ-GVL5Fz47q2c6yN3GzdZeYHT_FNT1jMysffBQ8MBPp-MX-kR_SjlvPzwe78WNgmEe2F.jpg?r=132",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80025678"
+  },
+
+  {
+    sname: "Space Force",
+    imgsrc: "https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZ5hoQboQj28YZmFqD6l8svq_iVGAu3WkDKLobvHHAB2hyvvD7LJIHZExGdnwVkBBbuPMJ7DVy6s6hoeQtFkM9hA89Rz02YZtbCUsJflf9MT-d-ovIkmhB7-S9QcAD7F3faV.jpg?r=fe2",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/81021929"
+  }
+];
+
+export default Sdata;
