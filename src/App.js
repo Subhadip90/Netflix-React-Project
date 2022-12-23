40e94afb2b9d32cd4e7943edff6fc579c053b7ba
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-23 15:33:48 +0000

    in cards.jsx file all the card props are passed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React from 'react'
+import Cards from './Components/Cards'
 
-function Card(props) {
-  console.log(props)
-  return (
-    <div>
-      <div className='cards'>
-        <div className="card">
-          <img src={props.imgsrc} alt='myPic' className='card_img' />
-          <div className="card_info">
-            <span className="card_category">{props.sname}</span>
-            <h3 className="card_title">{props.title}</h3>
-            <a href={props.link} target={'_blank'}>
-              <button>Watch Now</button>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 const App = () => {
   return (
     <>
-      <Card imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
+      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
+        sname="Money Heist"
+        link="https://www.netflix.com/in/title/80192098" />
+
+      <Cards imgsrc="https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSNtJvu8215Gl8GlVv_EEDFMgltlMgvQGUb_8wpfUL6BKCLHa5PYQcZZVizJJhcTCUsy4ST5mhGNGGffm-MYgGgdgvIGJf_OvT0IPUEke88IaN7ireJ4uV_BVLjVTXbwlfo1.jpg?r=c68"
+        sname="Stranger Things"
+        link="https://www.netflix.com/in/title/80192098" />
+
+      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
         title="A Netflix Original Series"
         sname="Money Heist"
         link="https://www.netflix.com/in/title/80192098" />
-      <Card />
-      <Card />
 
+      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
+        title="A Netflix Original Series"
+        sname="Money Heist"
+        link="https://www.netflix.com/in/title/80192098" />
+
+      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
+        title="A Netflix Original Series"
+        sname="Money Heist"
+        link="https://www.netflix.com/in/title/80192098" />
+
+      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
+        title="A Netflix Original Series"
+        sname="Money Heist"
+        link="https://www.netflix.com/in/title/80192098" />
     </>
   )
 }
