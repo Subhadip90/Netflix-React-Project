f5c4bb541bb8630a6502415bddf2262b7607db5b
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-24 14:36:29 +0000

    css style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Cards from './Components/Cards'
+import "./App.css";
 
 const App = () => {
   return (
     <>
+      <h1 className='heading-style'>List of top 5 Netflix in 2022</h1>
       <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
         sname="Money Heist"
         link="https://www.netflix.com/in/title/80192098" />
