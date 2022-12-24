95105085f55822da70e8cb453139f31ed2880bf1
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-24 15:11:23 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React from 'react'
 import Cards from './Components/Cards'
 import "./App.css";
+import Sdata from './Components/Sdata';
+
+// console.log(Sdata[0].sname)
 
 const App = () => {
   return (
     <>
       <h1 className='heading-style'>List of top 5 Netflix in 2022</h1>
-      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098" />
+      <Cards imgsrc={Sdata[0].imgsrc}
+        sname={Sdata[0].sname}
+        link={Sdata[0].link} />
 
-      <Cards imgsrc="https://occ-0-4157-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABSNtJvu8215Gl8GlVv_EEDFMgltlMgvQGUb_8wpfUL6BKCLHa5PYQcZZVizJJhcTCUsy4ST5mhGNGGffm-MYgGgdgvIGJf_OvT0IPUEke88IaN7ireJ4uV_BVLjVTXbwlfo1.jpg?r=c68"
-        sname="Stranger Things"
-        link="https://www.netflix.com/in/title/80192098" />
+      <Cards imgsrc={Sdata[1].imgsrc}
+        sname={Sdata[1].sname}
+        link={Sdata[1].link} />
 
-      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098" />
+      <Cards imgsrc={Sdata[2].imgsrc}
+        sname={Sdata[2].sname}
+        link={Sdata[2].link} />
 
-      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098" />
+      <Cards imgsrc={Sdata[3].imgsrc}
+        sname={Sdata[3].sname}
+        link={Sdata[3].link} />
 
-      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098" />
+      <Cards imgsrc={Sdata[4].imgsrc}
+        sname={Sdata[4].sname}
+        link={Sdata[4].link} />
 
-      <Cards imgsrc="https://occ-0-2232-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098" />
+      <Cards imgsrc={Sdata[5].imgsrc}
+        sname={Sdata[5].sname}
+        link={Sdata[5].link} />
     </>
   )
 }
-
 export default App
