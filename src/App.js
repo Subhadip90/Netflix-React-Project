6cc6d0d32e75e1780e666db5aeb4a02b21f62392
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-27 13:47:20 +0000

    done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import Netflix from './Components/Netflix';
 import Amazon from './Components/Amazon';
 
-const favSeries = "Netflix"
+// const favSeries = "Netflix"
 
 const App = () => {
   return (
     <>
       <h1 className='heading-style'>List of top 5 Netflix in 2022</h1>
-      {(favSeries === "Netflix") ? <Netflix /> : <Amazon />}
+      {/* {(favSeries === "Netflix") ? <Netflix /> : <Amazon />} */}
+
     </>
   )
 }
