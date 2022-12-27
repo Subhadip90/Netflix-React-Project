0dc4d365180ea3b071747ef2325163eeb265e720
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-27 13:45:12 +0000

    used ternary operator instead of if else

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import "./App.css";
 import Netflix from './Components/Netflix';
 import Amazon from './Components/Amazon';
 
-const favSeries = "Amazon"
-const FavS = () => {
-  if (favSeries === "Amazon") {
-    return <Netflix />
-  }
-  else {
-    return <Amazon />
-  }
-}
+const favSeries = "Netflix"
 
 const App = () => {
   return (
     <>
       <h1 className='heading-style'>List of top 5 Netflix in 2022</h1>
-      <FavS />
+      {(favSeries === "Netflix") ? <Netflix /> : <Amazon />}
     </>
   )
 }
